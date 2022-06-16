6cca1a7ab7fb011b85867b7f9a99f125c717beae
phanigeeth42 <geethvenkat42@gmail.com>
2022-06-16 11:36:40 +0000

    update

--- dataflow.sh
+++ dataflow.sh
@@ -1,3 +1,3 @@
-pip install -r requirements.txt
+pip3 install -r requirement.txt
 
-python test.py --runner=DataFlowRunner --project=named-inn-349004 --temp_location=gs://named-inn-349004/temp --region=us-central1 --job_name test
+python3 test.py --runner=DataFlowRunner --project=named-inn-349004 --temp_location=gs://named-inn-349004/temp --region=us-central1 --job_name test
