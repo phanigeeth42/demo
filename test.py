2c63e55f01891232d41ef6b1851e861d5a850051
phanigeeth42 <geethvenkat42@gmail.com>
2022-06-16 09:16:13 +0000

    update

--- test.py
+++ test.py
@@ -1,31 +1,19 @@
-from concurrent.futures import TimeoutError
-from google.cloud import pubsub_v1
-import os
+import apache_beam as beam
+from apache_beam.options.pipeline_options import GoogleCloudOptions
+from apache_beam.options.pipeline_options import SetupOptions
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import StandardOptions
 
-# TODO(developer)
-project_id = "named-inn-349004"
-subscription_id = "test_topic1-sub"
-# Number of seconds the subscriber should listen for messages
-timeout = 60.0
+options =PipelineOptions()
+options.view_as(StandardOptions).streaming=True
+p=beam.Pipeline(options=options)
+input_sub='projects/named-inn-349004/subscriptions/test-sub'
 
-subscriber = pubsub_v1.SubscriberClient()
-# The `subscription_path` method creates a fully qualified identifier
-# in the form `projects/{project_id}/subscriptions/{subscription_id}`
-subscription_path = subscriber.subscription_path(project_id, subscription_id)
+(
+    p
+    |beam.io.ReadFromPubSub(subscription=input_sub)
+    |beam.Map(print)
 
-def callback(message: pubsub_v1.subscriber.message.Message) -> None:
-    print(f"Received {message}.")
-    message.ack()
-
-streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
-print(f"Listening for messages on {subscription_path}..\n")
-
-# Wrap subscriber in a 'with' block to automatically call close() when done.
-with subscriber:
-    try:
-        # When `timeout` is not set, result() will block indefinitely,
-        # unless an exception is encountered first.
-        streaming_pull_future.result(timeout=timeout)
-    except TimeoutError:
-        streaming_pull_future.cancel()  # Trigger the shutdown.
-        streaming_pull_future.result()  # Block until the shutdown is complete.
+)
+result=p.run()
+result.wait_until_finish()
