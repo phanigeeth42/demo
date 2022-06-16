442daa44325a92aeac01ddb3f4f990ec3693b340
phanigeeth42 <geethvenkat42@gmail.com>
2022-06-16 13:23:04 +0000

    update

--- dataflow.sh
+++ dataflow.sh
@@ -1,3 +1,12 @@
 pip3 install -r requirement.txt
 
 python3 test.py --runner=DataFlowRunner --project=named-inn-349004 --temp_location=gs://named-inn-349004/temp --region=us-central1 --job_name test
+
+f=$(gcloud dataflow jobs list --filter="name=test" --region="us-central1" --status="active" | grep "STATE" | sed 's:.*/::')
+echo $f
+
+
+if [[ "$f" = 'STATE: Running' ]]
+then
+    echo "Dataflow Job created and Running" 
+fi
